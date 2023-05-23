a6f69e3e4d0c921c8bb14eefd5221d0f5aef9e20
NiteChill <puissantachille@gmail.com>
2023-05-23 21:39:32 +0200

    cleaning

--- src/assets/components/HeroHeader/HeroHeader.jsx
+++ src/assets/components/HeroHeader/HeroHeader.jsx
@@ -1,29 +1,11 @@
 import picture from '../../images/gsathan_picture.webp';
 import logo from '../../images/logo_gsathan_big.svg';
-import { useRef, useEffect, useState } from 'react';
 
 import styles from './HeroHeader.module.scss';
 
 export default function HeroHeader({ appInfo }) {
-  const heroHeaderRef = useRef(null);
-  const [isVisible, setIsVisible] = useState(false);
-  useEffect(() => {
-    if (
-      appInfo.scroll +
-        appInfo.ref.current.getBoundingClientRect().height -
-        heroHeaderRef.current.getBoundingClientRect().height / 4 >=
-      heroHeaderRef.current.offsetTop
-    ) {
-      setIsVisible(true);
-    } else {
-      setIsVisible(false);
-    }
-  }, [heroHeaderRef, appInfo]);
   return (
-    <div
-      className={`${isVisible && styles.visible} ${styles.container}`}
-      ref={heroHeaderRef}
-    >
+    <div className={`${styles.container}`}>
       <img src={picture} alt='picture' className={styles.image} />
       <div className={styles.logo_container}>
         <div className={styles.logo}>
